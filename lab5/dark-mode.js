049ae4bcb10848244a31d40dfbcc760c866c8670
Afonso Costa <afonsoccosta97@gmail.com>
2023-04-26 18:39:57 +0100

    done insert text and date

--- lab5/dark-mode.js
+++ lab5/dark-mode.js
@@ -4,15 +4,24 @@ document.addEventListener('DOMContentLoaded', function () {
     dataSet.theme = "light"
     var calculatorInput;
     const calculatorResult = document.querySelector('.resultado');
+    dateBuilder();
 
-    document.querySelector('.input-calculator').onchange = function () {
-
-        calculatorInput = this.value
-    }
     document.querySelector('#toggle-dark').onclick = function () {
 
         const inDarkMode = (dataSet.theme === 'dark');
         dataSet.theme = inDarkMode ? "light" : "dark";
+    }
+
+    document.querySelector('.user-name-input').onchange = function () {
+        const nameInput = this.value;
+        document.querySelector('.user-name-header').innerHTML = greetingMessageBuilder(nameInput);
+        document.querySelector('.user-name-footer').innerHTML = greetingMessageBuilder(nameInput);
+
+    }
+
+    document.querySelector('.input-calculator').onchange = function () {
+
+        calculatorInput = this.value
     }
 
     document.querySelector(".calculator-show-bt").onclick = function () {
@@ -31,4 +40,37 @@ document.addEventListener('DOMContentLoaded', function () {
         result = "";
         calculatorResult.innerHTML = `${result}`;
     }
+
+    document.querySelector('.text-to-center').onchange = function () {
+
+        document.querySelector('.centered-text').innerHTML = `${this.value}`;
+    }
 });
+
+function dateBuilder() {
+
+    const date = new Date();
+    const months = [
+        "janeiro", "fevereiro", "março", "abril",
+        "maio", "junho", "julho", "agosto",
+        "setembro", "outubro", "novembro", "dezembro"
+    ];
+    let month = months[date.getMonth()];
+    const dateFooter = document.querySelector('.date');
+    dateFooter.innerHTML = `${date.getDate()} de ${month}, ${date.getFullYear()}`;
+}
+
+function greetingMessageBuilder(name) {
+
+    const date = new Date();
+    const hour = date.getHours();
+
+    if(hour < 12) {
+
+        return "Bom dia " + name; 
+
+    } else {
+
+        return "Boa tarde " + name;
+    }
+}
