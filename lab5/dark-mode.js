cfc7430557e59d80ec1285b1493bc5b238602989
Afonso Costa <afonsoccosta97@gmail.com>
2023-04-26 17:04:30 +0100

    calculator with refresh problem

--- lab5/dark-mode.js
+++ lab5/dark-mode.js
@@ -2,9 +2,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const dataSet = document.documentElement.dataset;
     dataSet.theme = "light"
-    document.querySelector('#toggle-dark').onclick = function() {
-        
-        const inDarkMode = (dataSet.theme ==='dark');
+    var calculatorInput;
+    const calculatorResult = document.querySelector('.resultado');
+
+    document.querySelector('.input-calculator').onchange = function () {
+
+        calculatorInput = this.value
+    }
+    document.querySelector('#toggle-dark').onclick = function () {
+
+        const inDarkMode = (dataSet.theme === 'dark');
         dataSet.theme = inDarkMode ? "light" : "dark";
+    }
+
+    document.querySelector(".calculator-show-bt").onclick = function () {
+        if (calculatorInput === undefined) {
+
+            calculatorResult.innerHTML = "Insira uma expressão";
+
+        } else {
+            const result = eval(calculatorInput);
+            calculatorResult.innerHTML = `${result}`;
+        }
+    }
+
+    document.querySelector('.calculator-delete-bt').onclick = function () {
+
+        result = "";
+        calculatorResult.innerHTML = `${result}`;
     }
 });
